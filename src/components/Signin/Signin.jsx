cd03fd9d93dc2cc62e73b765255759dd51378f1d
Tanish-SR <srtanish2407@gmail.com>
2024-09-16 19:52:06 +0530

    changes in app.jsx

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -15,23 +15,41 @@ class Signin extends Component {
         this.setState({signInPassword: event.target.value})
     }
     onSubmitSignIn = () => {
+        // fetch('http://localhost:3000/signin', {
+        //   method: 'post',
+        //   headers: {'Content-Type': 'application/json'},
+        //   body: JSON.stringify({
+        //     email: this.state.signInEmail,
+        //     password: this.state.signInPassword
+        //   })
+        // })
+        //   .then(response => response.json())
+        //   .then(user => {
+        //     if (user.id) {
+        //       this.props.loadUser(user)
+        //       this.props.onRouteChange('home');
+        //     } else {
+        //         alert('wrong password or username')
+        //     }
+        //   })
         fetch('http://localhost:3000/signin', {
-          method: 'post',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            email: this.state.signInEmail,
-            password: this.state.signInPassword
-          })
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: this.state.signInEmail,
+                password: this.state.signInPassword
+            })
         })
-          .then(response => response.json())
-          .then(user => {
+        .then(response => response.json())
+        .then(user => {
             if (user.id) {
-              this.props.loadUser(user)
-              this.props.onRouteChange('home');
+                this.props.loadUser(user);
+                this.props.onRouteChange('home');
             } else {
-                alert('wrong password or username')
+                console.log('Login failed');
             }
-          })
+        });
+        
       }
     render() {
         const {onRouteChange} = this.props;
