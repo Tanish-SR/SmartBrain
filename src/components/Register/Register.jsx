f793d8780d8095b65774ef122a32dc1b26653ce6
Tanish-SR <srtanish2407@gmail.com>
2024-09-17 15:50:07 +0530

    changing the fetch urls

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -19,15 +19,15 @@ class Register extends Component {
         this.setState({password: event.target.value})
     }
     onSubmitRegister = () => {
-        fetch('http://localhost:3000/register', {
+        fetch(/*'http://localhost:3000/register'*/ 'https://smartbrain-api-1bn6.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password,
-                name: this.state.name
+              email: this.state.email,
+              password: this.state.password,
+              name: this.state.name
             })
-        })
+          })          
         .then(response => response.json())
         .then(user => {
             if (user.id){
